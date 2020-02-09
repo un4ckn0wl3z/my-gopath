7a7ebe2e1e91a4b17331dd9d6ecc0959e74153b3
un4ckn0wl3z <slickerth@gmail.com>
2020-02-09 17:54:25 +0700

    ding ding

--- 13_type_reference/main.go
+++ 13_type_reference/main.go
@@ -5,13 +5,14 @@ import "fmt"
 func main() {
 	var safe = true // declare without specific type
 	success := false
-
 	name := "Anuwat Khongchuai"
-
 	counter := 300
+
+	var degree = 0.3
 
 	fmt.Println(safe)
 	fmt.Println(success)
 	fmt.Println(name)
 	fmt.Println(counter)
+	fmt.Println(degree)
 }
