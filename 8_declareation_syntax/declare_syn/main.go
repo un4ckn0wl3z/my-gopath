67627264a17eb4d9e845d76b9fddebb8ce9f9ad4
un4ckn0wl3z <slickerth@gmail.com>
2020-02-09 13:59:10 +0700

    play with go declaration

---
+++ 8_declareation_syntax/declare_syn/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var speed int
+	var isReal bool
+	var radius float32
+	var pi float64
+	var name string
+
+	fmt.Println(speed)
+	fmt.Println(isReal)
+	fmt.Println(radius)
+	fmt.Println(pi)
+	fmt.Println(name)
+
+}
