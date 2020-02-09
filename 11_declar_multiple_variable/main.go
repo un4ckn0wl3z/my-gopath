7f7c06c35b1106a64730a024836679d5bcf794b5
un4ckn0wl3z <slickerth@gmail.com>
2020-02-09 14:25:10 +0700

    play with declare multiple variable

---
+++ 11_declar_multiple_variable/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		speed int
+
+		isReal bool
+		radius float32
+
+		pi   float64
+		name string
+	)
+
+	var firstname, lastname string
+
+	fmt.Println(speed)
+	fmt.Println(isReal)
+	fmt.Println(radius)
+	fmt.Println(pi)
+	fmt.Println(name)
+
+	fmt.Println(firstname)
+	fmt.Println(lastname)
+}
