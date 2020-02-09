34c1d76673314ec4385449ff5d58052f2ed948f5
un4ckn0wl3z <slickerth@gmail.com>
2020-02-09 14:06:50 +0700

    play with zero-value

---
+++ 9_zero_value/zero/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var speed int
+	var heat float64
+	var off bool
+	var brand string
+
+	fmt.Println(speed)
+	fmt.Println(heat)
+	fmt.Println(off)
+	fmt.Println(brand)
+	fmt.Printf("%q\n", brand)
+
+}
