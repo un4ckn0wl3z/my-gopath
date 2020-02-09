92c42658d3a513719b01effaa497e3128d0dd3a6
un4ckn0wl3z <slickerth@gmail.com>
2020-02-09 12:43:24 +0700

    add basic data type

---
+++ 7_basic_data_type/basic_01/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(
+		-200, -100, 0, 50, 100,
+	)
+
+	fmt.Println(
+		-50.5, -20.5, -0., 1., 100.56,
+	)
+
+	fmt.Println(
+		true, false,
+	)
+
+	fmt.Println(
+		" hello",
+		" ",
+		"hi",
+		"bye",
+		"สวัสดี",
+		"\n",
+		"ABC",
+	)
+}
