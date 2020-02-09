5133d61c07b120e96c55cb3dc5da30957aedefaf
un4ckn0wl3z <slickerth@gmail.com>
2020-02-09 15:00:55 +0700

    play with short declare

---
+++ 12_short_declaration/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var myAge, yourAge int
+
+	var temperature float64
+	var success bool
+	var language string
+
+	fmt.Println(myAge, yourAge)
+	fmt.Println(temperature)
+	fmt.Println(success)
+	fmt.Println(language)
+
+}
