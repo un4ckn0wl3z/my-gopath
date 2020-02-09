adbd4aa368f34baff584cc3ec480d3325184b9c8
un4ckn0wl3z <slickerth@gmail.com>
2020-02-09 20:42:09 +0700

    add multiple short declare

---
+++ 15_multiple_short_declare/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	safe, speed := true, 50
+	fmt.Println(safe, speed)
+
+	name, lastname := "Anuwat", "Khongchuai"
+	fmt.Println(name, lastname)
+
+	birth, death := 1995, 2099
+	fmt.Println(birth, death)
+
+	on, off := true, false
+	fmt.Println(on, off)
+
+}
