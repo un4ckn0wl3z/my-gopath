4934421da02953a0b3c782ad0a38c3cac6ff5302
un4ckn0wl3z <slickerth@gmail.com>
2020-02-09 17:51:14 +0700

    play with inferrence type

---
+++ 13_type_reference/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var safe = true // declare without specific type
+	success := false
+
+	name := "Anuwat Khongchuai"
+
+	counter := 300
+
+	fmt.Println(safe)
+	fmt.Println(success)
+	fmt.Println(name)
+	fmt.Println(counter)
+}
