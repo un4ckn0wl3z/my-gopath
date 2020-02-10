43a2b022a339cf91148eabe70ad95c4762500c6c
un4ckn0wl3z <slickerth@gmail.com>
2020-02-10 13:16:39 +0700

    add input from os.Args and introduce to slice datatype

---
+++ 25_input_from_terminal/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Printf("%#v\n", os.Args)
+
+	fmt.Println("Path:", os.Args[0])
+	fmt.Println("1st argument:", os.Args[1])
+	fmt.Println("2nd argument:", os.Args[2])
+	fmt.Println("3rd argument:", os.Args[3])
+
+	fmt.Println("Number of items inside os.Args:", len(os.Args))
+
+}
