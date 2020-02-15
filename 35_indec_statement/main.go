0ca89f46eb422e5a84af3fb92fc5d793535ca90c
un4ckn0wl3z <slickerth@gmail.com>
2020-02-15 12:02:07 +0700

    add incdec statement

---
+++ 35_indec_statement/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var n int
+	n = n + 1
+	fmt.Println(n)
+
+	n += 1
+	fmt.Println(n)
+
+	n++
+	fmt.Println(n)
+
+	n--
+	fmt.Println(n)
+
+	n = n - 1
+	fmt.Println(n)
+
+	n -= 1
+	fmt.Println(n)
+
+}
