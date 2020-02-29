432a45ece6cc45df2cccaf973910bf17b4641c2b
un4ckn0wl3z <slickerth@gmail.com>
2020-02-29 22:40:23 +0700

    play with loop

---
+++ 75_continue/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var (
+		sum int
+		i   = 1
+	)
+
+	for {
+		if i > 10 {
+			break
+		}
+
+		if i%2 != 0 {
+			i++
+			continue
+		}
+
+		sum += i
+		fmt.Println(i, "-->", sum)
+		i++
+	}
+
+}
