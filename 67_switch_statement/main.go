b6c7ea7911a6a6eca3c7c05b50f1bc39a2b7a68f
un4ckn0wl3z <slickerth@gmail.com>
2020-02-29 12:12:34 +0700

    play with basic switch

---
+++ 67_switch_statement/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	city := os.Args[1]
+	switch city {
+	case "Paris":
+		fmt.Println("France")
+		vip := true
+		fmt.Println("VIP trip?", vip)
+	case "Tokyo":
+		fmt.Println("Japan")
+	default:
+		fmt.Println("Where?")
+	}
+}
